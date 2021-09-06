7a9afb9f8f36a14bdf2b01a27871a81b10d82220
roberto117343 <roberto117343@gmail.com>
2021-09-06 14:57:58 +0200

    Create Anexo12.cpp

---
+++ Anexo12.cpp
@@ -0,0 +1,51 @@
+/**
+ * «Copyright 2021 Roberto Reinosa»
+ * 
+ * This program is free software: you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+    You should have received a copy of the GNU General Public License
+    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ * */
+
+#include <iostream>
+#include <fstream>
+#include <cmath>
+
+using namespace std;
+
+int main(){
+
+	ofstream archivo_guardado;
+	
+	archivo_guardado.open("1.dat", ios::out);
+
+	double x = 0.6;
+	
+	for(double r = 0.0; r < 18.0; r += 0.001){
+		
+		for(int i = 0; i < 1500; i++){
+		
+			x = r*x*(1.0-tanh(x));
+		
+			if(i > 1350){
+			
+				archivo_guardado << r << " " << x << endl;
+			
+			}
+		}
+		
+		x = 0.6;
+		
+	}
+	
+	archivo_guardado.close();
+	
+	return 0;
+	
+}
